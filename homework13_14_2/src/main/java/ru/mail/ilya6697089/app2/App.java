54c447cf8615782edf7bdb318ee073e80c01eb29
ilya-sakalouski <ilya6697089@mail.ru>
2020-02-14 17:51:24 +0300

    first commit

---
+++ homework13_14_2/src/main/java/ru/mail/ilya6697089/app2/App.java
@@ -0,0 +1,13 @@
+package ru.mail.ilya6697089.app2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
